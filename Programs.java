de36f240e58cd9da868d53b7fbac4a80c794f6b0
Priyanka <92361384+PRIYANKA643@users.noreply.github.com>
2023-02-04 23:31:43 +0530

    Add files via upload

---
+++ Programs.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author dell
+ */
+public class Program {
+    
+    public static void main(String[] args){
+        NewJFrame app = new NewJFrame();
+        app.setVisible(true);
+    }
+           
+}
